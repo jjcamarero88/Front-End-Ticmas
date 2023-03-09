d3a8ca0a5faa5210f91ac1d83d41b25c68152ac5
JAVI <90735937+jjcamarero88@users.noreply.github.com>
2023-03-09 10:07:30 -0300

    Add files via upload

---
+++ hobby.js
@@ -0,0 +1,3 @@
+document.getElementById("button").onclick = function (changetext) {
+    document.getElementById("hobbies").innerHTML = "Mis hobbies son jugar al futbol, leer novelas historicas y viajar por el mundo";
+}
